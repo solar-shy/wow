421fcb4bc876ed165255a9243178f9554f82761f
solar-shy <solar-shy@yandex.ru>
2020-10-08 19:10:42 +1000

    wow

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
+'''
 a = 10
 a -= 10
 print(a + 20 / 2 * 3 ** 2)
 
 b = 20 + 10 / 4 * 40
-print(b) 
+print(b)
 
 d = "a"
-name = " This is mt first code"
+name = " This is my first code"
 print(d + name)
+'''
+
+i = int(input("Введите первое число: "))
+j = int(input("Введите второе число: "))
+print(i + j)
